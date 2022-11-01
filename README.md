79dcadcebf9b6002a11497d150b7a7c6b29603f1
Devansh <devansh.subnani05@gmail.com>
2022-11-02 01:54:37 +0530

    Weather app using express and ejs
    Weather app using express and ejs

---
+++ README.md
@@ -0,0 +1,9 @@
+## Weather app using Express and ejs
+
+You can check current weather for every city in the world
+![weather app](.weatherApp.png)
+
+Run->node app.js
+
+
+https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=a3fa929a5a7fb4365ea46c103e2db342
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+// importing route
+const weatherRoute = require('./routes/weather');
+//adding ejs
+app.set('view engine', 'ejs');
+
+app.use('/', weatherRoute);
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log('server started'));
